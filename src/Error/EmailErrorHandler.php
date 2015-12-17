a2636dc66069d5f50a33fe010e44ef91d56d7242
Highstrike <highstrike@gmail.com>
2015-12-17 13:10:16 +0200

    email error handler fix

--- src/Error/EmailErrorHandler.php
+++ src/Error/EmailErrorHandler.php
@@ -2,15 +2,17 @@
 
 namespace Unimatrix\Utility\Error;
 
-use Unimatrix\Utility\Controller\Component\EmailComponent;
 use Unimatrix\Utility\Lib\Misc;
+use Unimatrix\Utility\Controller\Component\EmailComponent;
 use Cake\Controller\ComponentRegistry;
 use Cake\Error\ErrorHandler;
+use Cake\Core\Configure;
 use Exception;
 
 /**
  * Email Error Handler
  * Send a debug email for each fatal error or exception that is not catched.
+ * Note: Only works on live environments (debug = false)
  *
  * Usage exmaple (in bootstrap)
  * ----------------------------------------------------------------
@@ -18,7 +20,7 @@ use Exception;
  * replace with -> (new Unimatrix\Utility\Error\EmailErrorHandler(Configure::read('Error')))->register();
  *
  * @author Flavius
- * @version 0.1
+ * @version 0.2
  */
 class EmailErrorHandler extends ErrorHandler
 {
@@ -35,9 +37,15 @@ class EmailErrorHandler extends ErrorHandler
      */
     public function handleError($code, $description, $file = null, $line = null, $context = null) {
         // send a debug mail with the fatal error
-        if(!in_array($code, self::$_skipErrors)) {
+        if(Configure::read('debug') && !in_array($code, self::$_skipErrors)) {
             $mail = new EmailComponent(new ComponentRegistry());
-            $mail->debug('Application error', Misc::dump($exception, "Error Code {$code}", true));
+            $mail->debug('Application error', Misc::dump([
+                'code' => $code,
+                'description' => $description,
+                'file' => $file,
+                'line' => $line,
+                'context' => $context
+            ], $description, true));
         }
 
         // continue with error handle logic
@@ -50,9 +58,9 @@ class EmailErrorHandler extends ErrorHandler
      */
     public function handleException(Exception $exception) {
         // send a debug mail with the fatal exception
-        if(!in_array(get_class($exception), self::$_skipExceptions)) {
+        if(Configure::read('debug') && !in_array(get_class($exception), self::$_skipExceptions)) {
             $mail = new EmailComponent(new ComponentRegistry());
-            $mail->debug('Application exception', Misc::dump($exception, "Exception -> {$exception->getMessage()}", true));
+            $mail->debug('Application exception', Misc::dump($exception, $exception->getMessage(), true));
         }
 
         // continue with exception handle logic
