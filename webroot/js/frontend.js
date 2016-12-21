a5764ed55ad4ae5c7e83edb7e71867a03f8a1847
Highstrike <highstrike@gmail.com>
2016-12-21 15:13:44 +0200

    Bump version

--- webroot/js/frontend.js
+++ webroot/js/frontend.js
@@ -16,7 +16,7 @@ if(typeof DEV_ENV == 'undefined') DEV_ENV = true;
 // start frontend
 var Frontend = function() { 'use strict';
     var store = {
-    	version: '1.2',
+    	version: '1.3',
         js: {}
 
     // overwrite ajax settings
